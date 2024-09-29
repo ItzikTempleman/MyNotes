a2d171cd31765b47a1223a4658662d4160afa0b4
ItzikTempleman <itzik.templeman@gmail.com>
2024-09-29 16:42:56 +0300

    v
    v

--- app/src/main/java/com/itzik/mynotes/project/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/itzik/mynotes/project/ui/screens/ProfileScreen.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,27 +17,36 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Female
 import androidx.compose.material.icons.filled.Male
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Transgender
 import androidx.compose.material.icons.outlined.Call
-import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Cancel
 import androidx.compose.material.icons.outlined.DeleteOutline
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.PermContactCalendar
 import androidx.compose.material.icons.outlined.PowerSettingsNew
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -43,10 +54,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
 import com.itzik.mynotes.R
 import com.itzik.mynotes.project.main.NoteApp
@@ -71,6 +85,11 @@ fun ProfileScreen(
 ) {
     val user by userViewModel.publicUser.collectAsState()
 
+    var isImageOptionDialogOpen by remember {
+        mutableStateOf(false)
+    }
+
+
     val imagePickerLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri ->
             uri?.let {
@@ -90,13 +109,13 @@ fun ProfileScreen(
             .fillMaxSize()
             .background(Color.White)
     ) {
-        val (imageContainer, horizontalLine, verticalLine, uploadImageBtn, removeImageBtn, name, email, phone, gender, age, bottomColumn) = createRefs()
+        val (imageContainer, selectImageOptionCard, uploadImageBtn, name, email, phone, gender, age, bottomColumn) = createRefs()
 
         Box(
             modifier = Modifier
                 .constrainAs(imageContainer) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
+                    top.linkTo(parent.top, margin = 8.dp)
+                    start.linkTo(parent.start, margin = 8.dp)
                 }
                 .padding(16.dp)
                 .size(130.dp)
@@ -120,51 +139,86 @@ fun ProfileScreen(
                 )
             }
         }
-        HorizontalDivider(
-            modifier = Modifier
-                .constrainAs(horizontalLine) {
-                    top.linkTo(parent.top)
-                }
-                .padding(start = 8.dp, end = 8.dp, top = 50.dp),
-            color = Color.Transparent
-        )
 
-        VerticalDivider(
+        Box(
             modifier = Modifier
-                .constrainAs(verticalLine) {
-                    start.linkTo(parent.start)
-                }
-                .padding(start = 140.dp),
-            color = Color.Transparent
-        )
-
-        IconButton(
-            modifier = Modifier
-                .padding(start = 8.dp)
                 .constrainAs(uploadImageBtn) {
-                    top.linkTo(horizontalLine.bottom)
-                    start.linkTo(verticalLine.end)
-                },
-            onClick = {
-                imagePickerLauncher.launch("image/*")
-            }) {
-            Icon(imageVector = Icons.Outlined.Edit, contentDescription = null)
+                    end.linkTo(imageContainer.end, margin = 4.dp)
+                    bottom.linkTo(imageContainer.bottom, margin = 4.dp)
+                }
+                .padding(28.dp)
+                .size(36.dp)
+                .clip(CircleShape)
+                .background(Color.Transparent)
+                .border(BorderStroke(3.dp, Color.Red), CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            IconButton(
+                onClick = {
+                    isImageOptionDialogOpen = !isImageOptionDialogOpen
+                }
+            ) {
+                Icon(tint = Color.Red, imageVector = Icons.Outlined.Edit, contentDescription = null)
+            }
+        }
+        if (isImageOptionDialogOpen) {
+            Card(
+                modifier = Modifier.constrainAs(selectImageOptionCard) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }.padding(50.dp).wrapContentHeight(),
+                colors = CardDefaults.cardColors(Color.LightGray)
+
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Top,
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    IconButton(
+                        onClick = {
+                            isImageOptionDialogOpen=false
+                        }
+                    ) {
+                        Icon(
+                            contentDescription = null,
+                            imageVector = Icons.Outlined.Cancel
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                       Button(
+                           modifier = Modifier.padding(4.dp).weight(1f),
+                           shape= RoundedCornerShape(8.dp),
+                           colors=ButtonDefaults.buttonColors(Color.White),
+                            onClick = {
+                                imagePickerLauncher.launch("image/*")
+                            }
+                        ) {
+                            Text(stringResource(R.string.edit_profile_image), color = Color.Red, fontSize = 12.sp)
+                        }
+                        Button(
+                            modifier = Modifier.padding(4.dp).weight(1f),
+                            shape= RoundedCornerShape(8.dp),
+                            colors=ButtonDefaults.buttonColors(Color.White),
+                            onClick = {
+                                coroutineScope.launch {
+                                    userViewModel.updateProfileImage("")
+                                }
+                            }
+                        ) {
+                            Text(stringResource(R.string.remove_profile_image), fontSize = 12.sp, color = Color.Red)
+                        }
+                    }
+                }
+            }
         }
 
-        IconButton(
-            modifier = Modifier
-                .constrainAs(removeImageBtn) {
-                    top.linkTo(uploadImageBtn.top)
-                    bottom.linkTo(uploadImageBtn.bottom)
-                    start.linkTo(uploadImageBtn.end)
-                },
-            onClick = {
-                coroutineScope.launch {
-                    userViewModel.updateProfileImage("")
-                }
-            }) {
-            Icon(imageVector = Icons.Outlined.Delete, contentDescription = null)
-        }
 
         user?.let {
             Text(
@@ -371,4 +425,16 @@ fun ProfileScreen(
         }
 
     }
+}
+
+@Preview(showBackground = true, device = "spec:width=412dp,height=932dp")
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen(
+        coroutineScope = rememberCoroutineScope(),
+        rootNavController = rememberNavController(),
+        bottomBarNavController = rememberNavController(),
+        noteViewModel = viewModel(),
+        userViewModel = viewModel()
+    )
 }
