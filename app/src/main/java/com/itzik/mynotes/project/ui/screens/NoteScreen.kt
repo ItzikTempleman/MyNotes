91753e86ad99421623e19977b9bc779493292ad0
ItzikTempleman <itzik.templeman@gmail.com>
2024-10-02 19:54:03 +0300

    d
    e

--- app/src/main/java/com/itzik/mynotes/project/ui/screens/NoteScreen.kt
+++ app/src/main/java/com/itzik/mynotes/project/ui/screens/NoteScreen.kt
@@ -4,23 +4,28 @@ import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.PushPin
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -28,6 +33,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
@@ -76,7 +82,6 @@ fun NoteScreen(
         )
     }
 
-    var isBoldDialogOpen by remember { mutableStateOf(false) }
     var isColorPickerOpen by remember { mutableStateOf(false) }
     var fontSize by remember { mutableIntStateOf(note.fontSize) }
     var selectedColor by remember { mutableIntStateOf(note.fontColor) }
@@ -97,14 +102,15 @@ fun NoteScreen(
             .fillMaxSize()
             .background(Color.White)
     ) {
-        val (backBtn, fontSmaller, fontLarger, fontSizeIndicator, changeFontColorDialog, pin, star, colorPickerScreen, contentTF) = createRefs()
+        val (backBtn, fontEditRow, pin, star, colorPickerScreen, contentTF) = createRefs()
+
         GenericIconButton(
             modifier = Modifier
                 .constrainAs(backBtn) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                 }
-                .padding(8.dp)
+                .padding(start = 8.dp, top = 16.dp)
                 .size(32.dp),
             onClick = {
                 if (textFieldValue.text.isNotEmpty()) {
@@ -119,120 +125,160 @@ fun NoteScreen(
             colorNumber = 4
         )
 
-        Text(
-            modifier = Modifier
-                .constrainAs(fontSmaller) {
-                    top.linkTo(backBtn.top)
+        Card(
+            modifier = Modifier.padding(8.dp)
+                .constrainAs(fontEditRow) {
+                    top.linkTo(parent.top)
                     start.linkTo(backBtn.end)
-                    bottom.linkTo(backBtn.bottom)
                 }
-                .padding(start = 40.dp)
-                .clickable {
-                    if (fontSize > 10) {
-                        coroutineScope.launch {
-                            fontSize -= 2
-                            noteViewModel.updateSelectedNoteContent(
-                                textFieldValue.text,
-                                noteId = noteId,
-                                isStarred = note.isPinned,
-                                isPinned = note.isStarred,
-                                fontSize = fontSize,
-                                fontColor = note.fontColor,
-                                userId = note.userId,
+                .width(180.dp).height(45.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(12.dp),
+
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                Text(
+                    modifier = Modifier.padding(4.dp)
+                        .clickable {
+                            if (fontSize > 10) {
+                                coroutineScope.launch {
+                                    fontSize -= 2
+                                    noteViewModel.updateSelectedNoteContent(
+                                        textFieldValue.text,
+                                        noteId = noteId,
+                                        isStarred = note.isPinned,
+                                        isPinned = note.isStarred,
+                                        fontSize = fontSize,
+                                        fontColor = note.fontColor,
+                                        userId = note.userId,
+                                    )
+                                }
+                            }
+                        },
+                    text = "A",
+                    fontSize = 20.sp
+                )
+
+                Text(
+                    modifier = Modifier.padding(4.dp)
+                        .clickable {
+                            if (fontSize < 40) {
+                                coroutineScope.launch {
+                                    fontSize += 2
+                                    noteViewModel.updateSelectedNoteContent(
+                                        textFieldValue.text,
+                                        noteId = noteId,
+                                        isPinned = note.isPinned,
+                                        isStarred = note.isStarred,
+                                        fontSize = fontSize,
+                                        fontColor = note.fontColor,
+                                        userId = note.userId
+                                        )
+                                }
+                            }
+                        },
+                    text = "A",
+                    fontSize = 28.sp
+                )
+
+                VerticalDivider(modifier = Modifier.padding(12.dp))
+
+                Text(
+                    modifier = Modifier.clickable {
+                        if (textFieldValue.selection.start != textFieldValue.selection.end) {
+
+                            val selectedTextRange = textFieldValue.selection
+                            val selectedText = textFieldValue.text.substring(
+                                textFieldValue.selection.start,
+                                textFieldValue.selection.end
                             )
-                        }
-                    }
-                },
-            text = "A",
-            fontSize = 20.sp,
-            color = Color.Black
-        )
 
-        Text(
-            modifier = Modifier
-                .constrainAs(fontLarger) {
-                    top.linkTo(backBtn.top)
-                    start.linkTo(fontSmaller.end)
-                    bottom.linkTo(backBtn.bottom)
-                }
-                .padding(start = 8.dp)
-                .clickable {
-                    if (fontSize < 40) {
-                        coroutineScope.launch {
-                            fontSize += 2
-                            noteViewModel.updateSelectedNoteContent(
-                                textFieldValue.text,
-                                noteId = noteId,
-                                isPinned = note.isPinned,
-                                isStarred = note.isStarred,
-                                fontSize = fontSize,
-                                fontColor = note.fontColor,
-                                userId = note.userId,
-
+                            val annotatedString = buildAnnotatedString {
+                                append(
+                                    textFieldValue.text.substring(
+                                        0,
+                                        selectedTextRange.start
+                                    )
                                 )
-                        }
-                    }
-                },
-            fontWeight = FontWeight.Bold,
-            text = "A",
-            fontSize = 26.sp,
-            color = Color.Black
-        )
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                    append(selectedText)
+                                }
+                                append(textFieldValue.text.substring(selectedTextRange.end))
+                            }
 
-        Text(
-            modifier = Modifier
-                .constrainAs(fontSizeIndicator) {
-                    top.linkTo(backBtn.top)
-                    start.linkTo(fontLarger.end)
-                    bottom.linkTo(backBtn.bottom)
+                            textFieldValue = textFieldValue.copy(
+                                annotatedString = annotatedString,
+                                selection = TextRange.Zero
+                            )
+
+                            coroutineScope.launch {
+                                note.content = textFieldValue.text
+                                noteViewModel.updateSelectedNoteContent(
+                                    newChar = note.content,
+                                    userId = note.userId,
+                                    noteId = note.noteId,
+                                    isPinned = note.isPinned,
+                                    isStarred = note.isStarred,
+                                    fontSize = note.fontSize,
+                                    fontColor = note.fontColor
+                                )
+                            }
+                        }
+                    }.padding(4.dp),
+                    text = "B",
+                    fontSize = 20.sp
+                )
+                Text(
+                    modifier = Modifier.clickable {
+
+                    }.padding(4.dp),
+                    text = "B",
+                    fontSize = 20.sp, fontWeight = FontWeight.Bold
+                )
+
+                VerticalDivider(modifier = Modifier.padding(12.dp))
+
+                IconButton(
+                    modifier = Modifier.padding(4.dp),
+                    onClick = {
+                        isColorPickerOpen = !isColorPickerOpen
+                        focusManager.clearFocus()
+                    }) {
+                    Icon(
+                        modifier = Modifier.size(30.dp),
+                        painter = painterResource(id = R.drawable.color_palette),
+                        contentDescription = null,
+                        tint = Color.Unspecified
+                    )
                 }
-                .padding(start = 8.dp),
-            text = "$fontSize sp",
-            fontSize = 16.sp, color = Color.Black
-        )
-        IconButton(
-            modifier = Modifier
-                .constrainAs(changeFontColorDialog) {
-                    top.linkTo(backBtn.top)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(backBtn.bottom)
-                }
-                .padding(4.dp),
-            onClick = {
-                isColorPickerOpen = !isColorPickerOpen
-                focusManager.clearFocus()
-            }) {
-            Icon(
-                modifier = Modifier.size(30.dp),
-                painter = painterResource(id = R.drawable.color_palette),
-                contentDescription = null,
-                tint = Color.Unspecified
-            )
+            }
         }
+
 
         if (note.isPinned) {
             Icon(
-                modifier = Modifier
+                modifier = Modifier.padding(8.dp)
                     .constrainAs(pin) {
-                        top.linkTo(changeFontColorDialog.top)
-                        bottom.linkTo(changeFontColorDialog.bottom)
-                        end.linkTo(changeFontColorDialog.start)
+                        top.linkTo(parent.top)
+                        end.linkTo(star.start)
                     }
-                    .padding(horizontal = 44.dp)
                     .rotate(45f),
                 imageVector = Icons.Default.PushPin,
                 contentDescription = null,
                 tint = colorResource(id = R.color.light_purple)
             )
         }
+
         if (note.isStarred) {
             Icon(
-                modifier = Modifier
-                    .padding(horizontal = 4.dp)
+                modifier = Modifier.padding(8.dp)
                     .constrainAs(star) {
-                        top.linkTo(changeFontColorDialog.top)
-                        bottom.linkTo(changeFontColorDialog.bottom)
-                        end.linkTo(changeFontColorDialog.start)
+                        top.linkTo(parent.top)
+                        end.linkTo(parent.end)
                     },
                 imageVector = Icons.Default.Star,
                 contentDescription = null,
@@ -257,7 +303,7 @@ fun NoteScreen(
                         )
                 }
                 if (!newValue.selection.collapsed) {
-                    isBoldDialogOpen = true
+                   //TODO
                 }
             },
             colors = TextFieldDefaults.colors(
@@ -289,71 +335,6 @@ fun NoteScreen(
                 )
             }
         )
-
-
-        if (isBoldDialogOpen) {
-            AlertDialog(
-                onDismissRequest = {
-                    isBoldDialogOpen = false
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            if (textFieldValue.selection.start != textFieldValue.selection.end) {
-
-                                val selectedTextRange = textFieldValue.selection
-                                val selectedText = textFieldValue.text.substring(
-                                    textFieldValue.selection.start,
-                                    textFieldValue.selection.end
-                                )
-
-                                val annotatedString = buildAnnotatedString {
-                                    append(
-                                        textFieldValue.text.substring(
-                                            0,
-                                            selectedTextRange.start
-                                        )
-                                    )
-                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                                        append(selectedText)
-                                    }
-                                    append(textFieldValue.text.substring(selectedTextRange.end))
-                                }
-
-                                textFieldValue = textFieldValue.copy(
-                                    annotatedString = annotatedString,
-                                    selection = TextRange.Zero
-                                )
-
-                                coroutineScope.launch {
-                                    note.content = textFieldValue.text
-                                    noteViewModel.updateSelectedNoteContent(
-                                        newChar = note.content,
-                                        userId = note.userId,
-                                        noteId = note.noteId,
-                                        isPinned = note.isPinned,
-                                        isStarred = note.isStarred,
-                                        fontSize = note.fontSize,
-                                        fontColor = note.fontColor
-                                    )
-                                }
-                                isBoldDialogOpen = false
-                            }
-                        }
-                    ) {
-                        Text(text = "BOLD", fontWeight = FontWeight.Bold)
-                    }
-                }, dismissButton = {
-                    Button(onClick = {
-                        //TODO APPLY UN BOLD TEXT SELECTED
-                        isBoldDialogOpen = false
-                    }) {
-                        Text("UN-BOLD")
-                    }
-                }
-
-            )
-        }
 
         if (isColorPickerOpen) {
             ColorPickerDialog(
