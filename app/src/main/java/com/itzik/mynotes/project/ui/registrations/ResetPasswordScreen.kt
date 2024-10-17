71dfe1a62322855286b6ad9cfa37eb5514eefa52
ItzikTempleman <itzik.templeman@gmail.com>
2024-10-17 06:50:34 +0300

    f
    df

--- app/src/main/java/com/itzik/mynotes/project/ui/registrations/ResetPasswordScreen.kt
+++ app/src/main/java/com/itzik/mynotes/project/ui/registrations/ResetPasswordScreen.kt
@@ -1,10 +1,17 @@
 package com.itzik.mynotes.project.ui.registrations
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
+import com.itzik.mynotes.R
 import com.itzik.mynotes.project.viewmodels.UserViewModel
 import kotlinx.coroutines.CoroutineScope
 
@@ -15,10 +22,30 @@ fun ResetPasswordScreen(
     userViewModel: UserViewModel?,
 ) {
     ConstraintLayout(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
+        val (screenTitle, enterPhoneNumberText)= createRefs()
+        Text(
+            modifier = Modifier.constrainAs(screenTitle){
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+            }.padding(8.dp),
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            text = stringResource(R.string.reset_screen)
+        )
 
+        Text(
+            modifier = Modifier.constrainAs(enterPhoneNumberText){
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            fontSize = 22.sp,
+            text = stringResource(R.string.enter_phone_number_to_reset_with)
+        )
+
+        
     }
-
 }
